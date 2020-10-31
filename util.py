0508650d688983db368ad3860153ca63e9dfc5d3
Alex Seregin <alexeusgr@yahoo.com>
2020-10-31 10:41:00 +0300

    refactoring

--- util.py
+++ util.py
@@ -1,50 +1,41 @@
+def random_sample(period=10,sample=10):
+    import pandas as pd
+    import moex
+    tickers = pd.read_csv('./data/tickers.csv')
+    ticker = tickers['SECID'][0]
 
-import apimoex
-import pandas as pd
-import requests
 
-def get_tickers():
-    
-    request_url = ('https://iss.moex.com/iss/engines/stock/'
-                   'markets/shares/boards/TQBR/securities.json')
-    arguments = {'securities.columns': ('SECID,'
-                                        'REGNUMBER,'
-                                        'LOTSIZE,'
-                                        'SHORTNAME')}
-    with requests.Session() as session:
-        iss = apimoex.ISSClient(session, request_url, arguments)
-        data = iss.get()
-        df = pd.DataFrame(data['securities'])
-        df.set_index('SECID', inplace=True)
-
-        df.to_csv('tickers.csv')
-    
-def get_history(ticker='SNGSP'):    
-    with requests.Session() as session:
-        data = apimoex.get_board_history(session,ticker)
-        df = pd.DataFrame(data)
-        df.set_index('TRADEDATE', inplace=True)
+    df = pd.concat([moex.get_period(ticker,period ) for ticker in tickers['SECID']],axis = 1)
+    df = df.dropna(axis=1)
+    if sample != 0:
+        df = df.sample(sample,axis=1) 
     return df
 
-def fetch_data(tickers):
-    # assert file present, get_tickers() otherwise    
-    tickers = pd.read_csv('./data/tickers.csv')
+def calculate(df):
+    from pypfopt.expected_returns import mean_historical_return
+    from pypfopt.risk_models import CovarianceShrinkage
 
-    for ticker in tickers['SECID']:
+    mu = mean_historical_return(df)
+    S = CovarianceShrinkage(df).ledoit_wolf()
 
-        df = get_history(ticker)
-        df.to_csv('{}.csv'.format(ticker))
+    from pypfopt.efficient_frontier import EfficientFrontier
 
-def get_period(ticker, period=10):
-    '''Add normalization by lotsize'''
+    #ef = EfficientFrontier(mu, S)
+    #weights = ef.min_volatility()
+    #cleaned_weights = ef.clean_weights()
+    #print(ef.portfolio_performance(verbose=True))
+    #from pypfopt.discrete_allocation import DiscreteAllocation, get_latest_prices
 
-    tickers = pd.read_csv('./data/tickers.csv')
-    lotsize = tickers[tickers['SECID']==ticker]['LOTSIZE'].values[0]
-    df = pd.read_csv('./data/{}.csv'.format(ticker))#one timeseries
-    df.set_index('TRADEDATE', inplace=True)
-    df[ticker] = df['CLOSE']*lotsize
-    df = df[ticker].iloc[len(df)-period:]
-    return df
+    #latest_prices = get_latest_prices(df)
+    #da = DiscreteAllocation(weights, latest_prices, total_portfolio_value=20000)
+    #allocation, leftover = da.lp_portfolio()
+    #print(allocation)
+    
+    from pypfopt.plotting import plot_efficient_frontier
+    from pypfopt.cla import CLA
+    plot_efficient_frontier(CLA(mu,S))
+
+'''DEPRECATED'''
 def get_period_old(ticker, period=10):
     df = pd.read_csv('./data/{}.csv'.format(ticker))#one timeseries
     df.set_index('TRADEDATE', inplace=True)
@@ -67,7 +58,6 @@ def get_column(frames,colname='CLOSE'):
 #dt = [frame['CLOSE'] for frame in frames]
 #df = pd.DataFrame(data = dt,index=frames[0]["TRADEDATE"],columns= tickers['SECID'])
 
-# search 'timeseries plotting'
 # `
 #frame = frames[0]['CLOSE']
 #plt.figure()
@@ -75,29 +65,3 @@ def get_column(frames,colname='CLOSE'):
 #plt.show()
 
 
-
-
-## needs composed dataframe
-#from pypfopt.efficient_frontier import EfficientFrontier
-#
-#ef = EfficientFrontier(mu, S)
-#weights = ef.max_sharpe()
-#cleaned_weights = ef.clean_weights()
-#
-#print(cleaned_weights)
-## needs composed dataframe
-#from pypfopt.efficient_frontier import EfficientFrontier
-#
-#ef = EfficientFrontier(mu, S)
-#weights = ef.max_sharpe()
-#cleaned_weights = ef.clean_weights()
-#
-#print(cleaned_weights)
-## needs composed dataframe
-#from pypfopt.efficient_frontier import EfficientFrontier
-#
-#ef = EfficientFrontier(mu, S)
-#weights = ef.max_sharpe()
-#cleaned_weights = ef.clean_weights()
-#
-#print(cleaned_weights)
